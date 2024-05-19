5cc50f2e6bc7e7de124c42f765302e7f14e7fab1
jym0404 <jym090404@naver.com>
2024-05-19 23:39:16 +0900

    JSDoc

--- strani.js
+++ strani.js
@@ -27,6 +27,18 @@ function recursiveBackChange(callback, now, changeto, cduration, space, index){
         setTimeout(()=>{recursiveBackChange(callback, nres, changeto, cduration, space, index-1)},cduration)
     }
 }
+/**
+ * @callback printCallback
+ * @param {string} text The raw text to print
+*/
+/**
+ * @param {printCallback} callback The callback function to call when text is printed
+ * @param {string} original The original text already existed before 
+ * @param {string} changeto  The text to be printed
+ * @param {boolean} [reverse=false] When it is true, the animation printed in reverse
+ * @param {number} [cduration=30] The delay of one character to next as milliseconds
+ * @param {string} [space="&nbsp;"] The space character printed when it needs
+ */
 function strAnimate(callback, original, changeto, reverse=false, cduration=30, space=" "){
                                         //Space above is a nbsp, do not change it to a normal space
     if(reverse){
